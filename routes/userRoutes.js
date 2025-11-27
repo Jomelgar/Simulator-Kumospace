3f620e048801ba6ca3033ab636bc297c768a7363
bryanbanegas <140980028+bryanbanegas@users.noreply.github.com>
2025-11-26 22:36:54 -0600

    se guardan las contras hasheadas

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../controllers/userController");
 
 router.get("/getUsers", User.getUsers);
+router.get("/getUser/:id_user", User.getUser);
 router.post("/addUser", User.addUser);
 router.get("/inviteUser/:email", User.inviteUser);
 
