18c167b5e776591e3b88b0860db0d1c827050c6d
haroldstx <haroldhdiaz@unitec.edu>
2025-12-03 14:32:55 -0600

    Add cockies_backend, middleware_verify and config_morgan

--- routes/hiveRoutes.js
+++ routes/hiveRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Hive = require("../controllers/hiveController");
+const { VerifyToken } = require("../middleware/verifyToken");
 
+//Rutas que requieren autenticación
+router.use(VerifyToken);
 router.get("/getHives/:id_user", Hive.getHives);
 router.post("/createHive", Hive.createHive);
 router.post("/invitedToHive", Hive.invitedToHive);
