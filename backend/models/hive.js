f6d597041503825c932b344abb4f5b73611fd91e
bryanbanegas <140980028+bryanbanegas@users.noreply.github.com>
2025-12-03 09:39:55 -0600

    puro backend

---
+++ backend/models/hive.js
@@ -0,0 +1,23 @@
+const { DataTypes }=require("sequelize");
+const sequelize=require("../config/database");
+
+const Hive=sequelize.define("Hive", {
+    id_hive: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    hive_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    id_owner: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    tableName: "Hive",
+    timestamps: false
+});
+
+module.exports = Hive;
---
+++ backend/routes/hiveRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const Hive = require("../controllers/hiveController");
+
+router.get("/getHives/:id_user", Hive.getHives);
+router.post("/createHive", Hive.createHive);
+router.post("/invitedToHive", Hive.invitedToHive);
+router.get("/getPrivateRooms/:id_hive", Hive.getPrivateRooms);
+router.put("/updatePrivateRoom/:id_private_room", Hive.updatePrivateRoom);
+
+module.exports = router;
