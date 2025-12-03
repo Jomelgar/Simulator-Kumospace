cd89d5fb5886925ac54fee3a8b4407adcccc1579
Jomelgar <jomelgar1903@gmail.com>
2025-12-03 11:39:37 -0600

    implementacion de dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM node:20-alpine
 WORKDIR /app
 
 COPY package*.json ./
-RUN npm install
+RUN npm install concurrently
 COPY . .
 EXPOSE 3001
+EXPOSE 3002
 CMD [ "npm","start" ]
--- controllers/authController.js
+++ controllers/authController.js
@@ -10,6 +10,7 @@ exports.login= async ( req, res)=> {
             return res.status(404).json({ message: "User not found."});
         }
         const isMatch = await bcrypt.compare(password, user.password);
+        
         if(!isMatch){
             return res.status(400).json({ message: "Invalid credentials."});
         }  
